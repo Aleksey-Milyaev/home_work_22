b403d44ceb3c4ee5732585400aa0934f19ec7c7b
aleksey milyaev <trane94.milyaev@gmail.com>
2024-12-02 19:23:54 +0700

    finish home works 24

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def home(request):
-    return render(request, 'home.html')
+from .models import Category, Product
 
 
 def contacts(request):
@@ -12,3 +9,18 @@ def contacts(request):
         phone = request.POST.get('phone')
         return HttpResponse(f"Пользователь, {name} с номером {phone}! Ваше сообщение получено.")
     return render(request, 'contacts.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
+
+
+
